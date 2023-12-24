1aa856a075d235a44ff857fe798c0e8e71654686
Yash Sodavadiya <ysodavadiya@gmail.com>
2023-12-24 10:46:03 +0530

    home page start

--- index.php
+++ index.php
@@ -8,15 +8,23 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/home.css">
 </head>
 <body>
     <?php require('./components/navbar.php') ?>
 
     <div class="container">
         <div class="row">
-            <div class="col-sm-6">
-               
+            <div class="col-sm-7 cover1">
+               <div class="cover-img">
+                <img src="images/home/cover-bg.png" alt="">
+               </div>
+               <div class="cover-txt">
+                <p>SUIT FOR MAN</p>
+                <span>Fasion Trend</span>
+               </div>
+
             </div>
         </div>
     </div>
